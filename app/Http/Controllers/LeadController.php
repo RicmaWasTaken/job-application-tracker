0a8fa1069032c4197d0f6d98b02e705e8b715c33
Ricma <blaze9962@gmail.com>
2024-09-29 20:08:54 +0200

    Fix bug showing wrong date in leads.show

--- app/Http/Controllers/LeadController.php
+++ app/Http/Controllers/LeadController.php
@@ -15,7 +15,7 @@ class LeadController extends Controller
         $user_leads = Lead::where('user_id', $userId)->get();
         foreach($user_leads as $lead){
             //relative date handling (X days ago)
-            $days_ago = Carbon::parse($lead->last_contact)->diffForHumans();
+            $days_ago = Carbon::parse($lead->discovered_on)->diffForHumans();
             $lead->days_ago = $days_ago;
         }
         $successMessage = session()->get('successMessage');
@@ -23,6 +23,34 @@ class LeadController extends Controller
     }
 
     public function create(Request $request){
-        dd($request->all());
+        $validatedData = $request->validate([
+            'user_id' => 'required|integer',
+            'company_name' => 'required|string|max:255',
+            'location' => 'required|string|max:255',
+            'sector' => 'required|string|max:255',
+            'discovered_on' => 'required|date',
+            'via' => 'required|string|max:255',
+            'link' => 'nullable|url',
+            'comments' => 'nullable',
+            'quality' => 'required|integer|between:0,10',
+        ],[
+            'user_id.required' => 'Something went wrong, please try again later!',
+            'company_name.required' => 'Please enter a company name!',
+            'company_name.max' => 'Please enter a valid company name!',
+            'location.required' => 'Please enter a location!',
+            'location.max' => 'Please enter a valid location!',
+            'sector.required' => 'Please enter a sector!',
+            'sector.max' => 'Please enter a valid sector!',
+            'discovered_on.required' => 'Please enter a date of discovery!',
+            'date' => 'Please enter a valid date format (YYYY-MMM-DD)!',
+            'via.required' => 'Please enter a how you discovered the company (via)!',
+            'via.max' => 'Please enter a valid via!',
+            'quality.required' => 'Please enter a quality rating!',
+            'quality.integer' => 'Please enter a valid quality rating!',
+            'quality.between' => 'Please enter a quality rating between 0 and 10!',
+        ]);
+        Lead::create($validatedData);
+        $successMessage = 'Lead created successfully!';
+        return redirect()->route('leads.show')->with([ 'successMessage' => $successMessage ]);
     }
 }
