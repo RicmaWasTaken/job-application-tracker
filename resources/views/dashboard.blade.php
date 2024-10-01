ada1cafe5dc321967c0b66fd44d89711453ace8b
Ricma <blaze9962@gmail.com>
2024-10-01 22:52:12 +0200

    Add early-stage support for dashboard data visualisation Add external library for data visualisation (chart.js)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,6 +13,9 @@
                     <div class="flex flex-row bg-fuchsia-400 justify-between flex-1">
                         <div class="bg-red-500 w-7/12">
                             <p>Weekly stats</p>
+                            <div class="max-h-72 max-w-72">
+                                <canvas id="user-stats" width="200px" height="200px"></canvas>
+                            </div>
                         </div>
                         <div class="bg-blue-500 w-4/12">
                             <p>To-do</p>
@@ -25,4 +28,46 @@
             </div>
         </div>
     </div>
+    @if (Route::is('dashboard'))
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.4/dist/chart.umd.min.js"></script>
+    <script>
+        const ctx = document.getElementById('user-stats').getContext('2d');
+        const colors = {
+            accepted: 'green',
+            rejected: 'red',
+            waiting: '#FFA500',
+        }
+        const myDonutChart = new Chart(ctx, {
+            type: 'doughnut',
+            data: {
+                labels: ['Waiting', 'Accepted', 'Rejected'],
+                datasets: [{
+                    label: 'Applications',
+                    data: [20, 2, 9], // Bogus data to serve as a placeholder while I set up the controller function
+                    backgroundColor: [
+                        colors.waiting,
+                        colors.accepted,
+                        colors.rejected,
+                    ],
+                    borderColor: [
+                        colors.waiting,
+                        colors.accepted,
+                        colors.rejected,
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: true,
+                cutout: '50%', // Play with the donut effect 
+                plugins: {
+                    legend: {
+                        display: true,
+                        position: 'right', 
+                    },
+                }
+            }
+        });
+    </script>
+    @endif
 </x-app-layout>
