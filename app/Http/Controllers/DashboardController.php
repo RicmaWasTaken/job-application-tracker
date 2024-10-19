d7981c4359b88a077f557852e664a1f3616f96e7
Ricma <blaze9962@gmail.com>
2024-10-19 23:36:58 +0200

    Improve dashboard looks and functionality

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,25 +5,125 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Application;
+use App\Models\Lead;
 use Illuminate\Support\Carbon;
 
 class DashboardController extends Controller
 {
     public function show(){
         $user_id = Auth::id();
-        $lifetimeApplications = $this->getAllApplications($user_id);
-        $weeklyApplications = $this->getWeeklyApplications($user_id);
-        $sevenDaysAgo = Carbon::now()->subDays(7)->format('d/m');
-        $today = Carbon::now()->format('d/m');
-        $weeklyDates = "$sevenDaysAgo - $today";
+
+        // get data for charts under chartData variable
+        $chartData = new \stdClass();
+        $chartData->lifetimeApplications = json_encode($this->getApplicationsStatus($user_id, 'lifetime'));
+        $chartData->weeklyApplications = json_encode($this->getApplicationsStatus($user_id, 'weekly'));
+
+        //get data for stats table under statsData megavariable
+        $statsData = new \stdClass();
+        $statsData->weekly = new \stdClass();
+        $statsData->lifetime = new \stdClass();
+
+        //get data for weekly stats table 
+        $statsData->weekly->applicationTotal = $this->getApplicationCount($user_id, 'weekly');
+        $statsData->weekly->applicationEvolution = $this->getApplicationEvolution($user_id, 'weekly');
+        $statsData->weekly->applicationResponseRate = $this->getApplicationResponseRate($user_id, 'weekly');
+        $statsData->weekly->leadTotal = $this->getLeadCount($user_id, 'weekly');
+        $statsData->weekly->leadEvolution = $this->getLeadEvolution($user_id, 'weekly');
+
+        //get data for lifetime stats table
+        $statsData->lifetime->applicationTotal = $this->getApplicationCount($user_id, 'lifetime');
+        $statsData->lifetime->applicationEvolution = $this->getApplicationEvolution($user_id, 'lifetime');
+        $statsData->lifetime->applicationResponseRate = $this->getApplicationResponseRate($user_id, 'lifetime');
+        $statsData->lifetime->leadTotal = $this->getLeadCount($user_id, 'lifetime');
+        $statsData->lifetime->leadEvolution = $this->getLeadEvolution($user_id, 'lifetime');
+
+
+        // get data for dates under dates variable
+        $dates = new \stdClass();
+        $dates->sevenDaysAgo = Carbon::now()->subDays(7)->format('d/m');
+        $dates->today = Carbon::now()->format('d/m');
+        $dates->weeklyDates = "$dates->sevenDaysAgo - $dates->today";
+
         $lastApplications = $this->getLastApplications($user_id, 5);
-        return view('dashboard', compact('weeklyApplications', 'lastApplications', 'weeklyDates', 'lifetimeApplications'));
+
+
+        return view('dashboard', compact('chartData', 'statsData', 'dates', 'lastApplications'));
     }
 
-    public function getWeeklyApplications($user_id){
+    public function getApplicationCount($user_id, $timeframe){
+        if ($timeframe == 'weekly'){
+            $applications = Application::where('user_id', $user_id)->whereBetween('first_contact', [Carbon::now()->subDays(7), Carbon::now()])->count();
+        } else {
+            $applications = Application::where('user_id', $user_id)->count();
+        }
+        return $applications;
+    }
+    public function getLeadCount($user_id, $timeframe){
+        if ($timeframe == 'weekly'){
+            $leads = Lead::where('user_id', $user_id)->whereBetween('discovered_on', [Carbon::now()->subDays(7), Carbon::now()])->count();
+        } else {
+            $leads = Lead::where('user_id', $user_id)->count();
+        }
+        return $leads;
+    }
+    public function getApplicationResponseRate($user_id, $timeframe){
+        if ($timeframe == 'weekly'){
+            $applications = Application::where('user_id', $user_id)->whereBetween('first_contact', [Carbon::now()->subDays(7), Carbon::now()])->count();
+            $responses = Application::where('user_id', $user_id)->whereBetween('first_contact', [Carbon::now()->subDays(7), Carbon::now()])->where('status', 'accepted')->count();
+        } else {
+            $applications = Application::where('user_id', $user_id)->count();
+            $responses = Application::where('user_id', $user_id)->where('status', 'accepted')->count();
+        }
+        if ($applications == 0){
+            return 0;
+        } else {
+            return round(($responses / $applications) * 100, 2).'%';
+        }
+    }
+
+    public function getApplicationEvolution($user_id, $timeframe){
+        $today = Carbon::now();
+        $lastWeek = Carbon::now()->subDays(7);
+        $twoWeeksAgo = Carbon::now()->subDays(14);
+        if($timeframe == 'weekly'){
+            $applicationsLastWeek = Application::where('user_id', $user_id)->whereBetween('first_contact', [$lastWeek, $today])->count();
+            $applicationsTwoWeeksAgo = Application::where('user_id', $user_id)->whereBetween('first_contact', [$twoWeeksAgo, $twoWeeksAgo])->count();
+        } else {
+            return 'N/A';
+        }
+        if ($applicationsTwoWeeksAgo == 0){
+            return 0;
+        } else {
+            return round((($applicationsLastWeek - $applicationsTwoWeeksAgo) / $applicationsTwoWeeksAgo) * 100, 2).'%';
+        }
+    }
+    public function getLeadEvolution($user_id, $timeframe){
+        $today = Carbon::now();
+        $lastWeek = Carbon::now()->subDays(7);
+        $twoWeeksAgo = Carbon::now()->subDays(14);
+        if($timeframe == 'weekly'){
+            $leadsLastWeek = Lead::where('user_id', $user_id)->whereBetween('discovered_on', [$lastWeek, $today])->count();
+            $leadsTwoWeeksAgo = Lead::where('user_id', $user_id)->whereBetween('discovered_on', [$twoWeeksAgo, $twoWeeksAgo])->count();
+        } else {
+            return 'N/A';
+        }
+        if ($leadsTwoWeeksAgo == 0){
+            return 0;
+        } else {
+            return round((($leadsLastWeek - $leadsTwoWeeksAgo) / $leadsTwoWeeksAgo) * 100, 2).'%';
+        }
+    }
+
+
+    // chart methods
+    public function getApplicationsStatus($user_id, $timeframe){
         $processedApplications = [0, 0, 0]; //[accepted, rejected, waiting] -> make it simpler for rendering in js lol
-        $rawApplications = Application::where('user_id', $user_id)->whereBetween('first_contact', [Carbon::now()->subDays(7), Carbon::now()])->get();
-        foreach ($rawApplications as $application){
+        if ($timeframe == 'weekly'){
+            $rawApplications = Application::where('user_id', $user_id)->whereBetween('first_contact', [Carbon::now()->subDays(7), Carbon::now()])->get();
+        } else {
+            $rawApplications = Application::where('user_id', $user_id)->get();
+        }
+            foreach ($rawApplications as $application){
             if ($application->status == 'accepted'){
                 $processedApplications[0]++;
             } elseif ($application->status == 'rejected'){
@@ -35,21 +135,7 @@ class DashboardController extends Controller
         return $processedApplications;
     }
 
-    public function getAllApplications($user_id){
-        $processedApplications = [0, 0, 0]; //[accepted, rejected, waiting] -> make it simpler for rendering in js lol
-        $rawApplications = Application::where('user_id', $user_id)->get();
-        foreach ($rawApplications as $application){
-            if ($application->status == 'accepted'){
-                $processedApplications[0]++;
-            } elseif ($application->status == 'rejected'){
-                $processedApplications[1]++;
-            } else {
-                $processedApplications[2]++;
-            }
-        }
-        return $processedApplications;
-    }
-
+    // bottom row methods
     public function getLastApplications($user_id, $amount){
         $lastApplications = Application::where('user_id', $user_id)->orderBy('first_contact', 'desc')->take($amount)->get();
         foreach ($lastApplications as $application){
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,22 +10,83 @@
             <div class="bg-transparent overflow-hidden shadow-sm sm:rounded-lg flex-1">
                 <div class="p-6 text-gray-900 h-full flex flex-col gap-6 box-border">
                     <div class="flex flex-row justify-between flex-1 gap-6">
-                        <div class="relative bg-white sm:rounded-lg w-4/12">
-                            <div id="labels" class="flex flex-row">
-                                <p class="w-1/2 text-center hover:cursor-pointer rounded-tl-lg p-1">Weekly ({{$weeklyDates}})</p>
-                                <p class="w-1/2 text-center bg-slate-100 hover:cursor-pointer rounded-tr-lg p-1">Lifetime</p>
+                        <div class="flex flex-col relative bg-white sm:rounded-lg w-4/12">
+                            <div id="chart-labels" class="flex flex-row">
+                                <div class="flex flex-col justify-center items-center w-1/2 hover:cursor-pointer rounded-tl-lg p-1">
+                                    <p class="text-center">Weekly</p>
+                                    <p>({{$dates->weeklyDates}})</p>
+                                </div>
+                                <div class="flex flex-col justify-center items-center w-1/2 bg-slate-100 hover:cursor-pointer rounded-tr-lg p-1">
+                                    <p class="text-center">Lifetime</p>
+                                </div>
                             </div>
-                            <div id="chart-container" class="h-full relative">
-                                <div id="weekly-container" class="flex max-h-72 w-full justify-center">
+                            <div id="chart-container" class="flex-1 relative">
+                                <div id="weekly-chart-container" class="flex max-h-72 w-full justify-center">
                                     <canvas id="weekly-stats" width="200px" height="200px"></canvas>
                                 </div>
-                                <div id="lifetime-container" class="hidden max-h-72 w-full justify-center">
+                                <div id="lifetime-chart-container" class="hidden max-h-72 w-full justify-center">
                                     <canvas id="lifetime-stats" width="200px" height="200px"></canvas>
                                 </div>
-                            </div>                    
+                            </div>
                         </div>
-                        <div class="bg-white sm:rounded-lg p-4 w-4/12">
-                            <p>All-time stats A TABLE WITH NUMBERS LIKE MINECRAFT STATS SCREEN NOT A GRAPH</p>
+                        <div class="flex flex-col relative bg-white sm:rounded-lg w-4/12">
+                            <div id="stats-labels" class="flex flex-row">
+                                <div class="flex flex-col justify-center items-center w-1/2 hover:cursor-pointer rounded-tl-lg p-1">
+                                    <p class="text-center">Weekly</p>
+                                    <p>({{$dates->weeklyDates}})</p>
+                                </div>
+                                <div class="flex flex-col justify-center items-center w-1/2 bg-slate-100 hover:cursor-pointer rounded-tr-lg p-1">
+                                    <p class="text-center">Lifetime</p>
+                                </div>
+                            </div>
+                            <div id="stats-container" class="flex-1 relative p-4">
+                                <div id="weekly-stats-container" class="flex flex-col max-h-72 w-full justify-evenly">
+                                    <div class="flex flex-row w-full justify-between p-1">
+                                        <p>Application Total</p>
+                                        <p>{{$statsData->weekly->applicationTotal}}</p>
+                                    </div>
+                                    <div class="flex flex-row w-full justify-between p-1">
+                                        <p>Application evolution</p>
+                                        <p>{{$statsData->weekly->applicationEvolution}}</p>
+                                    </div>
+                                    <div class="flex flex-row w-full justify-between p-1">
+                                        <p>Application response rate</p>
+                                        <p>{{$statsData->weekly->applicationResponseRate}}</p>
+                                    </div>
+                                    <br>
+                                    <div class="flex flex-row w-full justify-between p-1">
+                                        <p>Lead Total</p>
+                                        <p>{{$statsData->weekly->leadTotal}}</p>
+                                    </div>
+                                    <div class="flex flex-row w-full justify-between p-1">
+                                        <p>Lead Evolution</p>
+                                        <p>{{$statsData->weekly->leadEvolution}}</p>
+                                    </div>
+                                </div>
+                                <div id="lifetime-stats-container" class="hidden flex-col max-h-72 w-full justify-evenly">
+                                <div class="flex flex-row w-full justify-between p-1">
+                                        <p>Application Total</p>
+                                        <p>{{$statsData->lifetime->applicationTotal}}</p>
+                                    </div>
+                                    <div class="flex flex-row w-full justify-between p-1">
+                                        <p>Application evolution</p>
+                                        <p>{{$statsData->lifetime->applicationEvolution}}</p>
+                                    </div>
+                                    <div class="flex flex-row w-full justify-between p-1">
+                                        <p>Application response rate</p>
+                                        <p>{{$statsData->lifetime->applicationResponseRate}}</p>
+                                    </div>
+                                    <br>
+                                    <div class="flex flex-row w-full justify-between p-1">
+                                        <p>Lead Total</p>
+                                        <p>{{$statsData->lifetime->leadTotal}}</p>
+                                    </div>
+                                    <div class="flex flex-row w-full justify-between p-1">
+                                        <p>Lead Evolution</p>
+                                        <p>{{$statsData->lifetime->leadEvolution}}</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                         <div class="bg-white sm:rounded-lg p-4 w-4/12">
                             <p>To-do <b>(feature not available yet)</b></p>
@@ -36,7 +97,7 @@
                         @if (isset($lastApplications))
                         <div class="flex flex-row h-[calc(100%-1.5rem)] max-w-[calc(100%-1.5rem)] gap-8">
                             @foreach ($lastApplications as $application)
-                            <div class="h-full flex-1 flex flex-col p-3 box-border sm:rounded-lg hover:bg-indigo-400 transition-all">
+                            <div class="h-full flex-1 flex flex-col p-3 box-border sm:rounded-lg border-y-4 border-transparent hover:border-indigo-400 transition-all">
                                 <a href="/applications/{{ $application->id }}" class="h-full w-full flex flex-col gap-6 justify-center">
                                     <p class="text-2xl text-center">{{$application->company_name}}</p>
                                     <p class=" text-lg text-center">{{$application->location}}</p>
@@ -53,51 +114,68 @@
     </div>
     <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.4/dist/chart.umd.min.js"></script>
     <script>
-        function switchChart() {
-            const weekly = document.getElementById('weekly-container');
-            const lifetime = document.getElementById('lifetime-container');
-            if (weekly.style.display === 'none') {
-                weekly.style.display = 'flex';
-                weeklyButton.classList.remove('bg-slate-100');
-                weeklyButton.classList.add('bg-white');
-                lifetime.style.display = 'none';
-                lifetimeButton.classList.remove('bg-white');
-                lifetimeButton.classList.add('bg-slate-100');
-            } else {
-                weekly.style.display = 'none';
-                weeklyButton.classList.remove('bg-white');
-                weeklyButton.classList.add('bg-slate-100');
-                lifetimeButton.classList.remove('bg-slate-100');
-                lifetimeButton.classList.add('bg-white');
-                lifetime.style.display = 'flex';
+        // ----- DATA ----- \\
+
+        const data = {
+            weekly : {{$chartData->weeklyApplications}}, //[accepted, rejected, waiting] -> follows alphabetical order
+            lifetime : {{$chartData->lifetimeApplications}}, //[accepted, rejected, waiting] -> follows alphabetical order
+        }
+
+        // ----- CHARTS ----- \\
+        const charts = {
+            buttons : {
+                weekly : document.getElementById('chart-labels').children[0],
+                lifetime : document.getElementById('chart-labels').children[1],
+            },
+            colors: {
+                accepted: '#5BC269',
+                rejected: '#E46773',
+                waiting: '#FCBA37',
+            },
+            containers: {
+                weekly : document.getElementById('weekly-chart-container'),
+                lifetime : document.getElementById('lifetime-chart-container'),
+            },
+            contexts: {
+                weekly : document.getElementById('weekly-stats').getContext('2d'),
+                lifetime : document.getElementById('lifetime-stats').getContext('2d'),
+            },
+            switch() {
+                if (charts.containers.weekly.style.display === 'none') {
+                    charts.containers.weekly.style.display = 'flex';
+                    charts.buttons.weekly.classList.remove('bg-slate-100');
+                    charts.buttons.weekly.classList.add('bg-white');
+                    charts.containers.lifetime.style.display = 'none';
+                    charts.buttons.lifetime.classList.remove('bg-white');
+                    charts.buttons.lifetime.classList.add('bg-slate-100');
+                } else {
+                    charts.containers.weekly.style.display = 'none';
+                    charts.buttons.weekly.classList.remove('bg-white');
+                    charts.buttons.weekly.classList.add('bg-slate-100');
+                    charts.buttons.lifetime.classList.remove('bg-slate-100');
+                    charts.buttons.lifetime.classList.add('bg-white');
+                    charts.containers.lifetime.style.display = 'flex';
+                }
             }
         }
 
-        const weeklyApplications = {!! json_encode($weeklyApplications) !!}; //[accepted, rejected, waiting] -> follows alphabetical order
-        const lifetimeApplications = {!! json_encode($lifetimeApplications) !!}
-        const ctxWeekly = document.getElementById('weekly-stats').getContext('2d');
-        const ctxLifetime = document.getElementById('lifetime-stats').getContext('2d');
-        const colors = {
-            accepted: '#5BC269',
-            rejected: '#E46773',
-            waiting: '#FCBA37',
-        }
-        const weeklyChart = new Chart(ctxWeekly, {
+        // chart creation
+        const weeklyChart = new Chart(charts.contexts.weekly, {
             type: 'doughnut',
             data: {
                 labels: ['Accepted', 'Rejected', 'Waiting'],
                 datasets: [{
                     label: 'Applications',
-                    data: weeklyApplications,
+                    data: data.weekly,
                     backgroundColor: [
-                        colors.accepted,
-                        colors.rejected,
-                        colors.waiting,
+                        charts.colors.accepted,
+                        charts.colors.rejected,
+                        charts.colors.waiting,
                     ],
                     borderColor: [
-                        colors.accepted,
-                        colors.rejected,
-                        colors.waiting,
+                        charts.colors.accepted,
+                        charts.colors.rejected,
+                        charts.colors.waiting,
                     ],
                     borderWidth: 1
                 }]
@@ -113,22 +191,22 @@
                 }
             }
         });
-        const lifetimeChart = new Chart(ctxLifetime, {
+        const lifetimeChart = new Chart(charts.contexts.lifetime, {
             type: 'doughnut',
             data: {
                 labels: ['Accepted', 'Rejected', 'Waiting'],
                 datasets: [{
                     label: 'Applications',
-                    data: lifetimeApplications, // Bogus data to serve as a placeholder while I set up the controller function
+                    data: data.lifetime,
                     backgroundColor: [
-                        colors.accepted,
-                        colors.rejected,
-                        colors.waiting,
+                        charts.colors.accepted,
+                        charts.colors.rejected,
+                        charts.colors.waiting,
                     ],
                     borderColor: [
-                        colors.accepted,
-                        colors.rejected,
-                        colors.waiting,
+                        charts.colors.accepted,
+                        charts.colors.rejected,
+                        charts.colors.waiting,
                     ],
                     borderWidth: 1
                 }]
@@ -145,10 +223,63 @@
             }
         });
 
-        const weeklyButton = document.getElementById('labels').children[0];
-        const lifetimeButton = document.getElementById('labels').children[1];
+        // chart button listeners
+        charts.buttons.weekly.addEventListener('click', function(){
+            if (charts.containers.weekly.style.display !== 'flex') {
+                charts.switch();
+            }
+        });
+        charts.buttons.lifetime.addEventListener('click', function(){
+            if (charts.containers.lifetime.style.display !== 'flex') {
+                charts.switch();
+            }
+        });
 
-        weeklyButton.addEventListener('click', switchChart);
-        lifetimeButton.addEventListener('click', switchChart);
+        // ----- STATS ----- \\
+
+        const stats = {
+            buttons : {
+                weekly : document.getElementById('stats-labels').children[0],
+                lifetime : document.getElementById('stats-labels').children[1],
+            },
+            colors: {
+                accepted: '#5BC269',
+                rejected: '#E46773',
+                waiting: '#FCBA37',
+            },
+            containers: {
+                weekly : document.getElementById('weekly-stats-container'),
+                lifetime : document.getElementById('lifetime-stats-container'),
+            },
+            switch() {
+                if (stats.containers.weekly.style.display === 'none') {
+                    stats.containers.weekly.style.display = 'flex';
+                    stats.buttons.weekly.classList.remove('bg-slate-100');
+                    stats.buttons.weekly.classList.add('bg-white');
+                    stats.containers.lifetime.style.display = 'none';
+                    stats.buttons.lifetime.classList.remove('bg-white');
+                    stats.buttons.lifetime.classList.add('bg-slate-100');
+                } else {
+                    stats.containers.weekly.style.display = 'none';
+                    stats.buttons.weekly.classList.remove('bg-white');
+                    stats.buttons.weekly.classList.add('bg-slate-100');
+                    stats.buttons.lifetime.classList.remove('bg-slate-100');
+                    stats.buttons.lifetime.classList.add('bg-white');
+                    stats.containers.lifetime.style.display = 'flex';
+                }
+            }
+        }
+
+        // stats buttons listeners
+        stats.buttons.weekly.addEventListener('click', function(){
+            if (stats.containers.weekly.style.display !== 'flex') {
+                stats.switch();
+            }
+        });
+        stats.buttons.lifetime.addEventListener('click', function(){
+            if (stats.containers.lifetime.style.display !== 'flex') {
+                stats.switch();
+            }
+        });
     </script>
 </x-app-layout>
