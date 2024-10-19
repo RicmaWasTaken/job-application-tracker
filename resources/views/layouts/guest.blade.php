ccec7c369a63f89f8ad07e7562226516b4fcc64a
Ricma <blaze9962@gmail.com>
2024-10-20 00:37:56 +0200

    Add logos and favicon

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,6 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="icon" href="{{ asset('favicon.png') }}" type="image/png">
+
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
